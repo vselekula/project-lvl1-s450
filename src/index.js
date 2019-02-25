d243e9edb3c7a93a2dd69a54350042c08aa478ba
vselekula <slavamarsmgn@gmail.com>
2019-02-25 22:40:44 +0300

    lvl2 on the go

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@ import readlineSync from 'readline-sync';
 const getName = () => {
     const userName = readlineSync.question('May I have your name? ');
     console.log('Hi ' + userName + '!');
-}
+};
 
-export const actual;
+export {getName};
