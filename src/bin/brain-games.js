6f20368fb1257dcca656be36bf37634f582fd1f4
Vjacheslav Petrov <slavamarsmgn@gmail.com>
2019-02-28 18:33:16 +0300

    ...

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import {getName} from '..';
+import greetings from '..';
 
-console.log('Welcome to the Brain Games!');
-
-getName();
+greetings();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync';
 
-export const getName = () => {
-    const userName = readlineSync.question('May I have your name? ');
-    console.log('Hi ' + userName + ' !');
+const greetings = () => {
+  console.log('Welcome to the Brain Games!');
+  const userName = readlineSync.question('May I have your name? ');
+  console.log('Hi ' + userName + ' !');
 };
 
+export default greetings;
