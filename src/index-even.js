280c6352d26bcc914e39009086c9d2e91fcf0bfb
Vjacheslav Petrov <slavamarsmgn@gmail.com>
2019-03-01 13:59:55 +0300

    step 4...

--- src/index-even.js
+++ src/index-even.js
@@ -1,31 +1,30 @@
 import readlineSync from 'readline-sync';
 
-const headerEven = () => console.log('Welcome to the Brain Games!\nAnswer "yes" if number even otherwise answer "no".');
+const headerEven = () => console.log('Welcome to the Brain Games!');
+const conditionEven = () => console.log('Answer "yes" if number even otherwise answer "no".');
 const askName = () => readlineSync.question('May I have your name? ');
 const greet = name => console.log(`Hi ${name} !`);
-const randomNumber = () => Math.floor(Math.random() * 10);
-const isEven = num => (num % 2 === 0 ? 'yes' : 'no');
+const randomNumber = (min, max) => Math.floor((Math.random() * (max - min) + min) * 10);
+const isEven = num => num % 2 === 0;
 const askNumber = num => readlineSync.question(`Is this number even: ${num} `);
 const isAnswerCorrect = (usersAnswer, rightAnswer) => usersAnswer === rightAnswer;
 
 const evenGame = () => {
   headerEven();
+  conditionEven();
   const name = askName();
   greet(name);
   let correctAnswersCounter = 0;
-  let questionsCounter = 1;
-  while (questionsCounter <= 3) {
-    const number = randomNumber();
-    const answer = askNumber(number);
-    const rightAnswer = isEven(number);
+  for (let i = 1; i <= 3; i += 1) {
+    const question = randomNumber(0, 10);
+    const answer = askNumber(question);
+    const rightAnswer = isEven(question) ? 'yes' : 'no';
     const result = isAnswerCorrect(answer, rightAnswer);
-    if (result === true) {
+    if (result) {
       correctAnswersCounter += 1;
-      questionsCounter += 1;
       console.log('Correct!');
     }
-    if (result === false) {
-      questionsCounter = 4;
+    if (!result) {
       console.log(`${answer} is wrong answer ;(. Correct answer was ${rightAnswer}\n Let's try again, ${name}`);
     }
     if (correctAnswersCounter === 3) {
